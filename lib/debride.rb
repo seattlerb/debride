528348fffaad1e9596adc46eb9ce17e357f4330a
Ryan Davis <ryand-ruby@zenspider.com>
2022-12-03 15:59:39 -0800

    prepped for release [git-p4: depot-paths = "//src/debride/dev/": change = 13602]

--- lib/debride.rb
+++ lib/debride.rb
@@ -12,7 +12,7 @@ require "path_expander"
 # A static code analyzer that points out possible dead methods.
 
 class Debride < MethodBasedSexpProcessor
-  VERSION = "1.9.0" # :nodoc:
+  VERSION = "1.10.1" # :nodoc:
   PROJECT = "debride"
 
   def self.load_plugins proj = PROJECT
